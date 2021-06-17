045ebed5490156b98111b60f1297bec8a99cc20b
Sam <anothercat1259@gmail.com>
2021-06-18 09:36:33 +1200

    enlarge map

--- assets/tilesheets/ice_with_water_tileset.tsx
+++ assets/tilesheets/ice_with_water_tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="ice_with_water_tileset" tilewidth="16" tileheight="16" tilecount="32" columns="8">
- <image source="ice_with_water_tileset.png" width="128" height="64"/>
+<tileset version="1.5" tiledversion="1.6.0" name="ice_with_water_tileset" tilewidth="128" tileheight="128" tilecount="32" columns="8">
+ <image source="ice_with_water_tileset.png" width="1024" height="512"/>
 </tileset>
