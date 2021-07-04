0d9ea1f5f31773945b8930cf25af455c3cee4d5d
Sam <anothercat1259@gmail.com>
2021-07-05 10:13:26 +1200

    add spring boards
    add spring board tilesheet
    edit tmx map
    add layer for springboard
    add extra jumping when on springboard
    fix typings

---
+++ assets/tilesheets/ice_with_water_tileset_springboard.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="ice_with_water_tileset_springboard" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="ice_with_water_tileset_springboard.png" width="512" height="512"/>
+</tileset>
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+from typing import List, Tuple
+
 import arcade
 
 TILE_WIDTH = 124
@@ -9,7 +11,8 @@ WIDTH = 10 * TILE_WIDTH
 HEIGHT = 6 * TILE_HEIGHT
 VIEWPORT_MARGIN = 140
 PLAYER_MOVEMENT_SPEED = 10
-PLAYER_JUMP_SPEED = 12
+PLAYER_JUMP_SPEED = 8
+BOOSTED_JUMP_SPEED = 12
 TITLE = "Ice Game"
 GRAVITY = 0.5
 
@@ -27,6 +30,7 @@ class Game(arcade.Window):
         self.wall_list: arcade.SpriteList
         self.player: arcade.Sprite
         self.physics_engine: arcade.PhysicsEnginePlatformer
+        self.spring_board_positions: List[Tuple[int, int]] = []
 
     def setup(self) -> None:
         self.player = arcade.Sprite("./assets/characters/placeholder_character.png")
@@ -40,12 +44,28 @@ class Game(arcade.Window):
         self.view_left = 0
 
     def load_map(self, resource: str) -> None:
-        layer_name = "ground"
+        tile_name = "ground"
+        spring_layer = "spring_boards"
         my_map = arcade.tilemap.read_tmx(resource)
 
         self.wall_list = arcade.tilemap.process_layer(
-            map_object=my_map, layer_name=layer_name, use_spatial_hash=True, scaling=0.5
+            map_object=my_map, layer_name=tile_name, use_spatial_hash=True, scaling=0.5
         )
+        spring_boards = arcade.tilemap.process_layer(
+            map_object=my_map,
+            layer_name=spring_layer,
+            use_spatial_hash=True,
+            scaling=0.5,
+        )
+        for spring_board in spring_boards:
+            self.wall_list.append(spring_board)
+            self.spring_board_positions.append(
+                (
+                    spring_board.center_x - (spring_board.width / 2),
+                    spring_board.center_y + (spring_board.height / 2),
+                )
+            )
+
         if my_map.background_color:
             arcade.set_background_color(my_map.background_color)
 
@@ -54,16 +74,33 @@ class Game(arcade.Window):
         self.wall_list.draw()
         self.player.draw()
 
-    def on_key_press(self, key, modifiers):
+    def calculate_jump_speed(self) -> int:
+        for pos in self.spring_board_positions:
+            colliding_x = (
+                pos[0] < self.player.center_x
+                and pos[0] + TILE_WIDTH > self.player.center_x
+            )
+            colliding_y = (
+                pos[1] < self.player.center_y
+                and pos[1] + TILE_HEIGHT > self.player.center_y
+            )
+            if colliding_x and colliding_y:
+                return BOOSTED_JUMP_SPEED
+
+        return PLAYER_JUMP_SPEED
+
+    def on_key_press(self, key: int, modifiers: int) -> None:
         if key == arcade.key.UP or key == arcade.key.W:
             if self.physics_engine.can_jump():
-                self.player.change_y = PLAYER_JUMP_SPEED
+                x = self.calculate_jump_speed()
+                logging.debug(x)
+                self.player.change_y = x
         elif key == arcade.key.LEFT or key == arcade.key.A:
             self.player.change_x = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player.change_x = PLAYER_MOVEMENT_SPEED
 
-    def on_key_release(self, key, modifiers):
+    def on_key_release(self, key: int, modifiers: int) -> None:
         if (
             key == arcade.key.LEFT
             or key == arcade.key.A
@@ -72,20 +109,17 @@ class Game(arcade.Window):
         ):
             self.player.change_x = 0
 
-    def death(self):
+    def death(self) -> None:
         self.setup()
 
-
-
-
-    def on_update(self, delta_time):
+    def on_update(self, delta_time: float) -> None:
         self.physics_engine.update()
         if self.player.center_y < self.player.height - 300:
-           self.death()
+            self.death()
         changed = False
 
         if self.player.center_x <= self.player.width / 2:
-            self.player.center_x = self.player.width / 2 
+            self.player.center_x = self.player.width / 2
             self.player.change_x = 0
 
         max_left_distance = self.view_left + VIEWPORT_MARGIN
@@ -97,17 +131,17 @@ class Game(arcade.Window):
         if self.player.right > max_left_distance:
             self.view_left += self.player.right - max_left_distance
             changed = True
-        
+
         max_bottom_distance = self.view_bottom + VIEWPORT_MARGIN
         if self.player.bottom < max_bottom_distance:
             self.view_bottom -= max_bottom_distance - self.player.bottom
             changed = True
-        
+
         max_top_distance = self.view_bottom + HEIGHT - VIEWPORT_MARGIN
         if self.player.top > max_top_distance:
             self.view_bottom += self.player.top - max_top_distance
             changed = True
-        
+
         if self.view_left <= 0:
             self.view_left = 0
         if self.view_bottom <= 0:
