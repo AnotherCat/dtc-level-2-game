392b460bfcf99d8aa3ce5719f69c8366fdefc525
Sam <anothercat1259@gmail.com>
2021-11-03 17:29:06 +1300

    clean up some tile issues
    Add spikes tileset with supporting tiles
    Add fill tiles to prevent 'empty' gaps

---
+++ assets/tilesheets/ice_with_water_tileset_spikes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="ice_with_water_tileset_spikes" tilewidth="128" tileheight="128" tilecount="20" columns="5">
+ <image source="ice_with_water_tileset_spikes.png" width="640" height="512"/>
+</tileset>
